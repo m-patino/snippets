16cd003eb48f06ab3fd9134b6e88682c8fc13247
m-patino <m.caudrelier@outlook.fr>
2024-01-14 15:27:52 +0100

    feat: add CsvHelper

---
+++ php/src/Helpers/CsvHelper.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Php\Helpers;
+
+use \Generator;
+use \Exception;
+
+class CsvHelper
+{
+    private static ?self $instance;
+
+    public static function create(): self{
+        return self::$instance ??= new static();
+    }
+
+    private function __construct()
+    {}
+
+    /**
+     * @param string $path
+     * @return Generator
+     * @throws Exception
+     */
+    public function getLines(string $path): Generator{
+        if($csv = fopen($path,"r")){
+            while (($line = fgetcsv($csv)) !== false) {
+                yield $line;
+            }
+        }else{
+            throw new  Exception("Error: invalid path \"$path\"");
+        }
+    }
+}
